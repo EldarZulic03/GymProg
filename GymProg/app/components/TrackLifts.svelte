9e62270d1b446c4325b3d98b8c615ac4eec90acd
Eldar <ezulic@uwo.ca>
2024-09-15 01:00:00 -0400

    added floating pop up box to track lifts page

--- GymProg/app/components/TrackLifts.svelte
+++ GymProg/app/components/TrackLifts.svelte
@@ -1,9 +1,18 @@
 <script>
+  let showBox = false;
+
+  function toggleBox() {
+    showBox = !showBox;
+    console.log("Toggle box, showBox is now:", showBox);
+  }
+
+  function handleBoxClick() {
+    console.log("Floating box clicked!");
+  }
 </script>
 
 <page>
   <gridLayout rows="*, auto" columns="*">
-    <!-- Your other content here -->
     <stackLayout row="0" col="0">
       <!-- Other content goes here -->
     </stackLayout>
@@ -21,6 +30,28 @@
       horizontalAlignment="right"
       verticalAlignment="bottom"
       margin="0 30 20 0"
+      on:tap={toggleBox}
     />
+
+    {#if showBox}
+      <absoluteLayout row="1" col="0" width="100%" height="100%" zIndex="1">
+        <label
+          text="This is a floating box."
+          backgroundColor="white"
+          padding="10"
+          borderColor="#ccc"
+          borderWidth="1"
+          borderRadius="5"
+          shadowColor="#000"
+          shadowOffset="0 2"
+          shadowOpacity="0.1"
+          shadowRadius="10"
+          left="70%"
+          width="200"
+          height="100"
+          on:tap={handleBoxClick}
+        />
+      </absoluteLayout>
+    {/if}
   </gridLayout>
 </page>
