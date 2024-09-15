0e10a72b635bd812f61088b3c55666720b1bccbb
Eldar <ezulic@uwo.ca>
2024-09-15 00:39:03 -0400

    Added skeleton for lifts and macros.

--- GymProg/app/components/Home.svelte
+++ GymProg/app/components/Home.svelte
@@ -1,5 +1,6 @@
 <script>
   import { isIOS, isAndroid } from "tns-core-modules/platform";
+  import TrackLifts from "./TrackLifts.svelte";
 </script>
 
 <page>
@@ -11,21 +12,20 @@
     <tabView flexGrow="0">
       <tabViewItem title="Track Your Lifts">
         <!-- Import Track your lifts page -->
+        <!-- <label textWrap="true">testing</label> -->
+        <frame> <TrackLifts /> </frame>
       </tabViewItem>
       <tabViewItem title="Track your macros">
         <!-- Import track your macros page -->
+        <label textWrap="true">Track your macros placeholder</label>
       </tabViewItem>
       <tabViewItem title="Calendar">
         <!-- Import calendar so people can go back and view their lifts and macros for that day -->
+        <label textWrap="true">Calendar placeholder</label>
       </tabViewItem>
     </tabView>
-
-    <!-- Content below TabView -->
-    <stackLayout flexGrow="1">
-      <!-- Additional content here if needed -->
-    </stackLayout>
-  </flexboxLayout>
-</page>
+  </flexboxLayout></page
+>
 
 <!-- 
 <style>
---
+++ GymProg/app/components/TrackLifts.svelte
@@ -0,0 +1,26 @@
+<script>
+</script>
+
+<page>
+  <gridLayout rows="*, auto" columns="*">
+    <!-- Your other content here -->
+    <stackLayout row="0" col="0">
+      <!-- Other content goes here -->
+    </stackLayout>
+
+    <button
+      row="1"
+      col="0"
+      text="+"
+      width="50"
+      height="50"
+      backgroundColor="#007AFF"
+      color="white"
+      borderRadius="25"
+      fontSize="24"
+      horizontalAlignment="right"
+      verticalAlignment="bottom"
+      margin="0 30 20 0"
+    />
+  </gridLayout>
+</page>
